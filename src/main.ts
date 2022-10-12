ff22598e63ce1ed45931e2533714bddddcc30545
Carlos Scovino <kikescovino@gmail.com>
2022-10-12 14:17:51 -0300

    feat: scroll based animation

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,23 @@ import {
   PerspectiveCamera,
   WebGLRenderer,
   Scene,
-  // TextureLoader,
-  // LoadingManager,
-  // Clock,
+  Mesh,
+  TorusGeometry,
+  ConeGeometry,
+  TorusKnotGeometry,
+  MeshToonMaterial,
+  DirectionalLight,
+  TextureLoader,
+  NearestFilter,
+  Clock,
+  Group,
   BufferGeometry,
   BufferAttribute,
   PointsMaterial,
-  AdditiveBlending,
   Points,
-  Color,
 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap';
 import * as dat from 'dat.gui';
-import { IParameters } from './types';
 import './style.css';
 
 // Sizes
@@ -54,6 +58,12 @@ window.addEventListener('mousemove', (event) => {
 
 // Debug
 const gui = new dat.GUI();
+const parameters = { materialColor: '#ffeded' };
+
+gui.addColor(parameters, 'materialColor').onChange(() => {
+  material.color.set(parameters.materialColor);
+  particlesMaterial.color.set(parameters.materialColor);
+});
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
@@ -61,176 +71,124 @@ const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
 // Scene
 const scene = new Scene();
 
-// Textures
-// const loadingManager = new LoadingManager();
-// const textureLoader = new TextureLoader(loadingManager);
+// Objects
+const textureLoader = new TextureLoader();
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg');
+gradientTexture.magFilter = NearestFilter;
 
-// Galaxy
-const parameters: IParameters = {
-  count: 10000,
-  size: 0.01,
-  radius: 5,
-  branches: 3,
-  spin: 1,
-  randomness: 0.2,
-  randomnessPower: 3,
-  insideColor: '#ff6030',
-  outsideColor: '#1b3984',
-};
+const material = new MeshToonMaterial({
+  color: parameters.materialColor,
+  gradientMap: gradientTexture,
+});
 
-let geometry: BufferGeometry | null = null;
-let material: PointsMaterial | null = null;
-let points: Points | null = null;
+const objectsDistance = 4;
 
-const generateGalaxy = ({
-  count,
-  size,
-  radius,
-  branches,
-  spin,
-  randomness,
-  randomnessPower,
-  insideColor,
-  outsideColor,
-}: IParameters) => {
-  if (points !== null) {
-    geometry?.dispose();
-    material?.dispose();
-    scene.remove(points);
-  }
+const mesh1 = new Mesh(new TorusGeometry(1, 0.4, 16, 60), material);
+const mesh2 = new Mesh(new ConeGeometry(1, 2, 32), material);
+const mesh3 = new Mesh(new TorusKnotGeometry(0.8, 0.35, 100, 16), material);
 
-  geometry = new BufferGeometry();
-  const positions = new Float32Array(count * 3);
-  const colors = new Float32Array(count * 3);
+mesh1.position.y = -objectsDistance * 0;
+mesh2.position.y = -objectsDistance * 1;
+mesh3.position.y = -objectsDistance * 2;
 
-  const colorInside = new Color(insideColor);
-  const colorOutside = new Color(outsideColor);
+mesh1.position.x = 2;
+mesh2.position.x = -2;
+mesh3.position.x = 2;
 
-  for (let i = 0; i < count; i++) {
-    const i3 = i * 3;
-    const radiusGalaxy = Math.random() * radius;
-    const spinAngle = radiusGalaxy * spin;
-    const branchAngle = ((i % branches) / branches) * Math.PI * 2;
+scene.add(mesh1, mesh2, mesh3);
 
-    const randomX =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
-    const randomY =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
-    const randomZ =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
+const sectionMeshes = [mesh1, mesh2, mesh3];
 
-    positions[i3] = Math.cos(branchAngle + spinAngle) * radiusGalaxy + randomX;
-    positions[i3 + 1] = randomY;
-    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radiusGalaxy + randomZ;
+// Particles
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3);
 
-    const mixedColor = colorInside.clone().lerp(colorOutside, radiusGalaxy / radius);
+for (let i = 0; i < particlesCount; i++) {
+  positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+  positions[i * 3 + 1] =
+    objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length;
+  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+}
 
-    colors[i3] = mixedColor.r;
-    colors[i3 + 1] = mixedColor.g;
-    colors[i3 + 2] = mixedColor.b;
-  }
+const particlesGeometry = new BufferGeometry();
+particlesGeometry.setAttribute('position', new BufferAttribute(positions, 3));
 
-  geometry.setAttribute('position', new BufferAttribute(positions, 3));
-  geometry.setAttribute('color', new BufferAttribute(colors, 3));
+const particlesMaterial = new PointsMaterial({
+  color: parameters.materialColor,
+  sizeAttenuation: true,
+  size: 0.03,
+});
 
-  material = new PointsMaterial({
-    size,
-    sizeAttenuation: true,
-    depthWrite: false,
-    blending: AdditiveBlending,
-    vertexColors: true,
-  });
+const particles = new Points(particlesGeometry, particlesMaterial);
+scene.add(particles);
 
-  points = new Points(geometry, material);
-  scene.add(points);
-};
-
-generateGalaxy(parameters);
-
-gui
-  .add(parameters, 'count')
-  .min(100)
-  .max(1000000)
-  .step(100)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'size')
-  .min(0.001)
-  .max(0.1)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'radius')
-  .min(0.01)
-  .max(20)
-  .step(0.01)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'branches')
-  .min(2)
-  .max(20)
-  .step(1)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'spin')
-  .min(-5)
-  .max(5)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'randomness')
-  .min(0)
-  .max(2)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'randomnessPower')
-  .min(1)
-  .max(10)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui.addColor(parameters, 'insideColor').onFinishChange(() => generateGalaxy(parameters));
-gui.addColor(parameters, 'outsideColor').onFinishChange(() => generateGalaxy(parameters));
+// Lights
+const directionalLight = new DirectionalLight('#ffffff', 1);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
 
 // Camera
-const camera = new PerspectiveCamera(75, aspectRatio, 0.1, 100);
-camera.position.x = 3;
-camera.position.y = 3;
-camera.position.z = 3;
-scene.add(camera);
+const cameraGroup = new Group();
+scene.add(cameraGroup);
 
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+const camera = new PerspectiveCamera(35, aspectRatio, 0.1, 100);
+camera.position.z = 6;
+cameraGroup.add(camera);
 
 // Renderer
 const renderer = new WebGLRenderer({
   canvas,
+  alpha: true,
 });
 renderer.setSize(SIZES.width, SIZES.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+// Scroll
+let scrollY = window.scrollY;
+let currentSection = 0;
+
+window.addEventListener('scroll', () => {
+  scrollY = window.scrollY;
+  const newSection = Math.round(scrollY / SIZES.height);
+
+  if (newSection !== currentSection) {
+    currentSection = newSection;
+
+    gsap.to(sectionMeshes[currentSection].rotation, {
+      duration: 1.5,
+      ease: 'power2.inOut',
+      x: '+=6',
+      y: '+=3',
+      z: '+=1.5',
+    });
+  }
+});
+
 // Clock
-// const clock = new Clock();
+const clock = new Clock();
+let previousTime = 0;
 
 // Animations
 const tick = () => {
   // Clock
-  // const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
-  // Update particles
+  // Animate camera
+  camera.position.y = (-scrollY / SIZES.height) * objectsDistance;
 
-  // Update controls
-  controls.update();
+  const parallaxX = cursor.x * 0.5;
+  const parallaxY = cursor.y * 0.5;
+
+  cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
+  cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
+
+  // Animate meshes
+  sectionMeshes.forEach((mesh) => {
+    mesh.rotation.x += deltaTime * 0.1;
+    mesh.rotation.y += deltaTime * 0.12;
+  });
 
   // Render
   renderer.render(scene, camera);
