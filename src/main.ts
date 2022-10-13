7e569552b30a8ddcfc2a7c010a235de23a6ef761
Carlos Scovino <kikescovino@gmail.com>
2022-10-13 15:10:04 -0300

    feat: add physics

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,22 @@ import {
   PerspectiveCamera,
   WebGLRenderer,
   Scene,
-  // TextureLoader,
-  // LoadingManager,
-  // Clock,
-  BufferGeometry,
-  BufferAttribute,
-  PointsMaterial,
-  AdditiveBlending,
-  Points,
-  Color,
+  PCFSoftShadowMap,
+  CubeTextureLoader,
+  Mesh,
+  MeshStandardMaterial,
+  PlaneGeometry,
+  AmbientLight,
+  DirectionalLight,
+  Clock,
+  SphereGeometry,
+  Vector3,
+  BoxGeometry,
+  Quaternion,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'dat.gui';
-import { IParameters } from './types';
+import * as CANNON from 'cannon-es';
 import './style.css';
 
 // Sizes
@@ -54,6 +57,46 @@ window.addEventListener('mousemove', (event) => {
 
 // Debug
 const gui = new dat.GUI();
+const debuggObject = {
+  createSphere: () => {
+    createSphere(
+      Math.random() * 0.5,
+      {
+        x: (Math.random() - 0.5) * 3,
+        y: 3,
+        z: (Math.random() - 0.5) * 3,
+      },
+      scene,
+      world,
+    );
+  },
+  createBox: () => {
+    createBox(
+      Math.random() * 0.5,
+      Math.random() * 0.5,
+      Math.random() * 0.5,
+      {
+        x: (Math.random() - 0.5) * 3,
+        y: 3,
+        z: (Math.random() - 0.5) * 3,
+      },
+      scene,
+      world,
+    );
+  },
+  reset: () => {
+    objectsToUpdate.forEach((object) => {
+      object.body.removeEventListener('collide', playHitSound);
+      world.removeBody(object.body);
+
+      scene.remove(object.mesh);
+    });
+    objectsToUpdate.splice(0, objectsToUpdate.length);
+  },
+};
+gui.add(debuggObject, 'createSphere');
+gui.add(debuggObject, 'createBox');
+gui.add(debuggObject, 'reset');
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
@@ -62,150 +105,84 @@ const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
 const scene = new Scene();
 
 // Textures
-// const loadingManager = new LoadingManager();
-// const textureLoader = new TextureLoader(loadingManager);
+const cubeTextureLoader = new CubeTextureLoader();
 
-// Galaxy
-const parameters: IParameters = {
-  count: 10000,
-  size: 0.01,
-  radius: 5,
-  branches: 3,
-  spin: 1,
-  randomness: 0.2,
-  randomnessPower: 3,
-  insideColor: '#ff6030',
-  outsideColor: '#1b3984',
+const environmentMapTexture = cubeTextureLoader.load([
+  '/textures/environmentMaps/0/px.png',
+  '/textures/environmentMaps/0/nx.png',
+  '/textures/environmentMaps/0/py.png',
+  '/textures/environmentMaps/0/ny.png',
+  '/textures/environmentMaps/0/pz.png',
+  '/textures/environmentMaps/0/nz.png',
+]);
+
+// Sounds
+const hitSound = new Audio('/sounds/hit.mp3');
+const playHitSound = (collision: CANNON.ICollisionEvent) => {
+  const impactStrength = collision.contact.getImpactVelocityAlongNormal();
+  if (impactStrength > 1.5) {
+    hitSound.volume = Math.random();
+    hitSound.currentTime = 0;
+    hitSound.play();
+  }
 };
 
-let geometry: BufferGeometry | null = null;
-let material: PointsMaterial | null = null;
-let points: Points | null = null;
+// Physics
+const world = new CANNON.World();
+world.broadphase = new CANNON.SAPBroadphase(world);
+world.allowSleep = true;
+world.gravity.set(0, -9.82, 0);
 
-const generateGalaxy = ({
-  count,
-  size,
-  radius,
-  branches,
-  spin,
-  randomness,
-  randomnessPower,
-  insideColor,
-  outsideColor,
-}: IParameters) => {
-  if (points !== null) {
-    geometry?.dispose();
-    material?.dispose();
-    scene.remove(points);
-  }
+const defaultMaterial = new CANNON.Material('default');
+const defaultContactMaterial = new CANNON.ContactMaterial(defaultMaterial, defaultMaterial, {
+  friction: 0.1,
+  restitution: 0.7,
+});
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
 
-  geometry = new BufferGeometry();
-  const positions = new Float32Array(count * 3);
-  const colors = new Float32Array(count * 3);
+const floorShape = new CANNON.Plane();
+const floorBody = new CANNON.Body();
+floorBody.mass = 0;
+floorBody.addShape(floorShape);
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+world.addBody(floorBody);
 
-  const colorInside = new Color(insideColor);
-  const colorOutside = new Color(outsideColor);
+// Objects
 
-  for (let i = 0; i < count; i++) {
-    const i3 = i * 3;
-    const radiusGalaxy = Math.random() * radius;
-    const spinAngle = radiusGalaxy * spin;
-    const branchAngle = ((i % branches) / branches) * Math.PI * 2;
+// Floor
+const floor = new Mesh(
+  new PlaneGeometry(10, 10),
+  new MeshStandardMaterial({
+    color: '#777777',
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5,
+  }),
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 
-    const randomX =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
-    const randomY =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
-    const randomZ =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
+// Lights
+const ambientLight = new AmbientLight('#ffffff', 0.7);
+scene.add(ambientLight);
 
-    positions[i3] = Math.cos(branchAngle + spinAngle) * radiusGalaxy + randomX;
-    positions[i3 + 1] = randomY;
-    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radiusGalaxy + randomZ;
-
-    const mixedColor = colorInside.clone().lerp(colorOutside, radiusGalaxy / radius);
-
-    colors[i3] = mixedColor.r;
-    colors[i3 + 1] = mixedColor.g;
-    colors[i3 + 2] = mixedColor.b;
-  }
-
-  geometry.setAttribute('position', new BufferAttribute(positions, 3));
-  geometry.setAttribute('color', new BufferAttribute(colors, 3));
-
-  material = new PointsMaterial({
-    size,
-    sizeAttenuation: true,
-    depthWrite: false,
-    blending: AdditiveBlending,
-    vertexColors: true,
-  });
-
-  points = new Points(geometry, material);
-  scene.add(points);
-};
-
-generateGalaxy(parameters);
-
-gui
-  .add(parameters, 'count')
-  .min(100)
-  .max(1000000)
-  .step(100)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'size')
-  .min(0.001)
-  .max(0.1)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'radius')
-  .min(0.01)
-  .max(20)
-  .step(0.01)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'branches')
-  .min(2)
-  .max(20)
-  .step(1)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'spin')
-  .min(-5)
-  .max(5)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'randomness')
-  .min(0)
-  .max(2)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'randomnessPower')
-  .min(1)
-  .max(10)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui.addColor(parameters, 'insideColor').onFinishChange(() => generateGalaxy(parameters));
-gui.addColor(parameters, 'outsideColor').onFinishChange(() => generateGalaxy(parameters));
+const directionalLight = new DirectionalLight('#ffffff', 0.2);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
 
 // Camera
 const camera = new PerspectiveCamera(75, aspectRatio, 0.1, 100);
-camera.position.x = 3;
-camera.position.y = 3;
-camera.position.z = 3;
+camera.position.set(-3, 3, 3);
 scene.add(camera);
 
 // Controls
@@ -216,18 +193,102 @@ controls.enableDamping = true;
 const renderer = new WebGLRenderer({
   canvas,
 });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFSoftShadowMap;
 renderer.setSize(SIZES.width, SIZES.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+// Utils
+const objectsToUpdate: Array<{ mesh: Mesh; body: CANNON.Body }> = [];
+
+const sphereGeometry = new SphereGeometry(1, 20, 20);
+const sphereMaterial = new MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createSphere = (
+  radius: number,
+  position: { x: number; y: number; z: number },
+  scene: Scene,
+  world: CANNON.World,
+) => {
+  const mesh = new Mesh(sphereGeometry, sphereMaterial);
+  mesh.scale.set(radius, radius, radius);
+  mesh.castShadow = true;
+  mesh.position.copy(new Vector3(position.x, position.y, position.z));
+  scene.add(mesh);
+
+  const shape = new CANNON.Sphere(radius);
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(new CANNON.Vec3(position.x, position.y, position.z));
+  body.addEventListener('collide', playHitSound);
+  world.addBody(body);
+
+  objectsToUpdate.push({ mesh, body });
+};
+
+createSphere(0.5, { x: 0, y: 3, z: 0 }, scene, world);
+
+const boxGeometry = new BoxGeometry(1, 1, 1);
+const boxMaterial = new MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createBox = (
+  width: number,
+  height: number,
+  depth: number,
+  position: { x: number; y: number; z: number },
+  scene: Scene,
+  world: CANNON.World,
+) => {
+  const mesh = new Mesh(boxGeometry, boxMaterial);
+  mesh.scale.set(width, height, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(new Vector3(position.x, position.y, position.z));
+  scene.add(mesh);
+
+  const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5));
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(new CANNON.Vec3(position.x, position.y, position.z));
+  body.addEventListener('collide', playHitSound);
+  world.addBody(body);
+
+  objectsToUpdate.push({ mesh, body });
+};
+
 // Clock
-// const clock = new Clock();
+const clock = new Clock();
+let oldElapsedTime = 0;
 
 // Animations
 const tick = () => {
   // Clock
-  // const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - oldElapsedTime;
+  oldElapsedTime = elapsedTime;
 
-  // Update particles
+  // Update physics world
+  world.step(1 / 60, deltaTime, 3);
+
+  objectsToUpdate.forEach((object) => {
+    object.mesh.position.copy(new Vector3(...Object.values(object.body.position)));
+    object.mesh.quaternion.copy(new Quaternion(...Object.values(object.body.quaternion)));
+  });
 
   // Update controls
   controls.update();
