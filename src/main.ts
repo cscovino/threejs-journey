d707acf6e3705a2b4571261f80e988ad82719666
Carlos Scovino <kikescovino@gmail.com>
2022-10-13 18:53:07 -0300

    feat: import 3d models

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,19 @@ import {
   PerspectiveCamera,
   WebGLRenderer,
   Scene,
-  // TextureLoader,
-  // LoadingManager,
-  // Clock,
-  BufferGeometry,
-  BufferAttribute,
-  PointsMaterial,
-  AdditiveBlending,
-  Points,
-  Color,
+  Mesh,
+  PlaneGeometry,
+  MeshStandardMaterial,
+  AmbientLight,
+  DirectionalLight,
+  PCFSoftShadowMap,
+  AnimationMixer,
+  Clock,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import * as dat from 'dat.gui';
-import { IParameters } from './types';
 import './style.css';
 
 // Sizes
@@ -61,173 +61,83 @@ const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
 // Scene
 const scene = new Scene();
 
-// Textures
-// const loadingManager = new LoadingManager();
-// const textureLoader = new TextureLoader(loadingManager);
+// Models
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/');
 
-// Galaxy
-const parameters: IParameters = {
-  count: 10000,
-  size: 0.01,
-  radius: 5,
-  branches: 3,
-  spin: 1,
-  randomness: 0.2,
-  randomnessPower: 3,
-  insideColor: '#ff6030',
-  outsideColor: '#1b3984',
-};
+let mixer: AnimationMixer | null = null;
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+gltfLoader.load('/models/Fox/glTF/Fox.gltf', (gltf) => {
+  // const children = [...gltf.scene.children];
+  // children.forEach((child) => {
+  //   scene.add(child);
+  // });
 
-let geometry: BufferGeometry | null = null;
-let material: PointsMaterial | null = null;
-let points: Points | null = null;
+  mixer = new AnimationMixer(gltf.scene);
+  const action = mixer.clipAction(gltf.animations[1]);
+  action.play();
 
-const generateGalaxy = ({
-  count,
-  size,
-  radius,
-  branches,
-  spin,
-  randomness,
-  randomnessPower,
-  insideColor,
-  outsideColor,
-}: IParameters) => {
-  if (points !== null) {
-    geometry?.dispose();
-    material?.dispose();
-    scene.remove(points);
-  }
+  gltf.scene.scale.set(0.025, 0.025, 0.025);
+  scene.add(gltf.scene);
+});
 
-  geometry = new BufferGeometry();
-  const positions = new Float32Array(count * 3);
-  const colors = new Float32Array(count * 3);
+// FLoor
+const floor = new Mesh(
+  new PlaneGeometry(10, 10),
+  new MeshStandardMaterial({ color: '#444444', metalness: 0, roughness: 0.5 }),
+);
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 
-  const colorInside = new Color(insideColor);
-  const colorOutside = new Color(outsideColor);
+// Lights
+const ambientLight = new AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
 
-  for (let i = 0; i < count; i++) {
-    const i3 = i * 3;
-    const radiusGalaxy = Math.random() * radius;
-    const spinAngle = radiusGalaxy * spin;
-    const branchAngle = ((i % branches) / branches) * Math.PI * 2;
-
-    const randomX =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
-    const randomY =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
-    const randomZ =
-      Math.pow(Math.random(), randomnessPower) *
-      (Math.random() < 0.5 ? 1 : -1) *
-      randomness *
-      radiusGalaxy;
-
-    positions[i3] = Math.cos(branchAngle + spinAngle) * radiusGalaxy + randomX;
-    positions[i3 + 1] = randomY;
-    positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radiusGalaxy + randomZ;
-
-    const mixedColor = colorInside.clone().lerp(colorOutside, radiusGalaxy / radius);
-
-    colors[i3] = mixedColor.r;
-    colors[i3 + 1] = mixedColor.g;
-    colors[i3 + 2] = mixedColor.b;
-  }
-
-  geometry.setAttribute('position', new BufferAttribute(positions, 3));
-  geometry.setAttribute('color', new BufferAttribute(colors, 3));
-
-  material = new PointsMaterial({
-    size,
-    sizeAttenuation: true,
-    depthWrite: false,
-    blending: AdditiveBlending,
-    vertexColors: true,
-  });
-
-  points = new Points(geometry, material);
-  scene.add(points);
-};
-
-generateGalaxy(parameters);
-
-gui
-  .add(parameters, 'count')
-  .min(100)
-  .max(1000000)
-  .step(100)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'size')
-  .min(0.001)
-  .max(0.1)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'radius')
-  .min(0.01)
-  .max(20)
-  .step(0.01)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'branches')
-  .min(2)
-  .max(20)
-  .step(1)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'spin')
-  .min(-5)
-  .max(5)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'randomness')
-  .min(0)
-  .max(2)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui
-  .add(parameters, 'randomnessPower')
-  .min(1)
-  .max(10)
-  .step(0.001)
-  .onFinishChange(() => generateGalaxy(parameters));
-gui.addColor(parameters, 'insideColor').onFinishChange(() => generateGalaxy(parameters));
-gui.addColor(parameters, 'outsideColor').onFinishChange(() => generateGalaxy(parameters));
+const directionalLight = new DirectionalLight(0xffffff, 0.6);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
+scene.add(directionalLight);
 
 // Camera
 const camera = new PerspectiveCamera(75, aspectRatio, 0.1, 100);
-camera.position.x = 3;
-camera.position.y = 3;
-camera.position.z = 3;
+camera.position.set(2, 2, 2);
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 0.75, 0);
 controls.enableDamping = true;
 
 // Renderer
 const renderer = new WebGLRenderer({
   canvas,
 });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFSoftShadowMap;
 renderer.setSize(SIZES.width, SIZES.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Clock
-// const clock = new Clock();
+const clock = new Clock();
+let previousTime = 0;
 
 // Animations
 const tick = () => {
   // Clock
-  // const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
 
-  // Update particles
+  // Update mixer
+  if (mixer !== null) mixer.update(deltaTime);
 
   // Update controls
   controls.update();
