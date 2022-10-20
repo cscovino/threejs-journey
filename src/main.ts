8e763ef6d071dc832833b518dffc5f4a8a8c457d
Carlos Scovino <kikescovino@gmail.com>
2022-10-20 01:10:57 -0300

    feat: shaders patterns

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,13 @@ import {
   PerspectiveCamera,
   WebGLRenderer,
   Scene,
-  TextureLoader,
   PlaneGeometry,
   Mesh,
-  RawShaderMaterial,
-  // BufferAttribute,
-  Vector2,
   Clock,
-  Color,
+  ShaderMaterial,
+  DoubleSide,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import * as dat from 'dat.gui';
 import testVertexShader from './shaders/test/vertex.glsl';
 import testFragmentShader from './shaders/test/fragment.glsl';
 import './style.css';
@@ -37,55 +33,27 @@ window.addEventListener('resize', () => {
   renderer.setSize(SIZES.width, SIZES.height);
 });
 
-window.addEventListener('dblclick', () => {
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen();
-  } else {
-    document.exitFullscreen();
-  }
-});
-
-// Debug
-const gui = new dat.GUI();
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl') as HTMLCanvasElement;
 
 // Scene
 const scene = new Scene();
 
-// Textures
-const textureLoader = new TextureLoader();
-const flagTexture = textureLoader.load('/textures/flag-french.jpg');
-
-// Mesh
+// Geometry
 const geometry = new PlaneGeometry(1, 1, 32, 32);
 
-// const count = geometry.attributes.position.count;
-// const randoms = new Float32Array(count);
-
-// for (let i = 0; i < count; i++) {
-//   randoms[i] = Math.random();
-// }
-
-// geometry.setAttribute('aRandom', new BufferAttribute(randoms, 1));
-
-const material = new RawShaderMaterial({
+// Material
+const material = new ShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
+  side: DoubleSide,
   uniforms: {
-    uFrequency: { value: new Vector2(10, 5) },
     uTime: { value: 0 },
-    uColor: { value: new Color('orange') },
-    uTexture: { value: flagTexture },
   },
 });
 
-gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX');
-gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY');
-
+// Mesh
 const mesh = new Mesh(geometry, material);
-mesh.scale.y = 2 / 3;
 scene.add(mesh);
 
 // Camera
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -1,22 +1,13 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
-uniform vec2 uFrequency;
+#define PI 3.1415926535897932384626433832795
+
 uniform float uTime;
 
-attribute vec3 position;
-attribute vec2 uv;
-
 varying vec2 vUv;
-varying float vElevation;
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-
-  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
-
-  modelPosition.z += elevation;
+  modelPosition.x += sin(modelPosition.z * PI + uTime) + cos(modelPosition.z * PI + uTime);
+  modelPosition.z += sin(modelPosition.x * PI + uTime) + cos(modelPosition.x * PI + uTime);
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
@@ -24,5 +15,4 @@ void main() {
   gl_Position = projectedPosition;
 
   vUv = uv;
-  vElevation = elevation;
 }
